bb72dfa35f8eaae07d59f6ab1802b9dbec2464fd
Benjamin Kolstrup <benzokolstrup@gmail.com>
2022-02-10 20:07:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ window.addEventListener('mousedown', (e) => {
         dragElement(e.target.closest('.dragable'), e.clientY, e.clientX);
     }
 });
+console.log(JSON.parse(localStorage.getItem('1')));
+var obj = {
+    top: null,
+    left: null,
+    id: null
+}
 function dragElement(element, heightPos, widthPos){
     document.addEventListener('mousemove', startDragElement);
     document.addEventListener('mouseup', stopDragElement);
@@ -11,8 +17,16 @@ function dragElement(element, heightPos, widthPos){
     function startDragElement(e){
         element.style.top = (e.clientY - elementTopPos) + 'px';
         element.style.left = (e.clientX - elementLeftPos) + 'px';
+        obj = {
+            top: (e.clientY - elementTopPos),
+            left: (e.clientX - elementLeftPos),
+            id: e.target.textContent
+        }
     };
     function stopDragElement(){
         document.removeEventListener('mousemove', startDragElement);
+        window.localStorage.setItem(obj.id, JSON.stringify(obj));
     };
 }
+
+
